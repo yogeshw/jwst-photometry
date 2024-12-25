673b4bffc91d184ebf611f0674a84d83352f4025
Yogesh G Wadadekar <yogesh@ncra.tifr.res.in>
2024-12-25 12:12:36 +0530

    Add unit test for detecting sources with Gaussian functions
    * Create a mock image with Gaussian sources
    * Use the `detect_sources` function to detect sources in the mock image
    * Assert that the detected objects and background are of the correct types and that at least one object is detected

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -41,5 +41,19 @@ class TestDetection(unittest.TestCase):
         star_candidates = identify_star_candidates_advanced(objects, f200w_image, f160w_image)
         self.assertIsInstance(star_candidates, list)
 
+    def test_detect_sources_with_gaussian(self):
+        # Create a mock image with Gaussian sources
+        self.image = np.random.normal(loc=0.0, scale=1.0, size=(100, 100))
+        for _ in range(10):
+            x, y = np.random.randint(0, 100, size=2)
+            width = np.random.uniform(1, 3)
+            amplitude = np.random.uniform(5, 10)
+            self.image += amplitude * np.exp(-((np.arange(100)[:, None] - x)**2 + (np.arange(100)[None, :] - y)**2) / (2 * width**2))
+        
+        objects, bkg = detect_sources(self.image, self.detection_params)
+        self.assertIsInstance(objects, np.ndarray)
+        self.assertIsInstance(bkg, sep.Background)
+        self.assertGreater(len(objects), 0)
+
 if __name__ == '__main__':
     unittest.main()
